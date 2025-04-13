ebbf0eb0d02e14f737e284bdf65bf33e21f0318c
milad-sol <lvlilad_soltani@yahoo.com>
2025-04-13 11:52:59 +0330

    add show users projects in users profile

--- users/views.py
+++ users/views.py
@@ -1,8 +1,12 @@
 from django.contrib.auth.models import User
+from django.db.models import Q
 from rest_framework import generics, permissions, status
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import RefreshToken
+
+from project.serializers import ProjectSerializer
 from .serializers import UserSerializer, UserRegistrationSerializer
+from project.models import Project
 
 
 # Create your views here.
@@ -31,3 +35,15 @@ class UserProfileView(generics.RetrieveUpdateAPIView):
 
     def get_object(self):
         return self.request.user
+
+    def retrieve(self, request, *args, **kwargs):
+        user = self.get_object()
+        user_serializer = self.get_serializer(user)
+        projects = Project.objects.filter(Q(owner=user) | Q(members=user))
+        data = user_serializer.data
+        if projects.exists():
+            projects_serializer = ProjectSerializer(projects, many=True)
+            data['projects'] = projects_serializer.data
+        else:
+            data['projects'] = []
+        return Response(data)

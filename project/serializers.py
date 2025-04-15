29152cedc174672b0d013adc9958e7199bb729e1
milad-sol <lvlilad_soltani@yahoo.com>
2025-04-15 11:22:59 +0330

    change project serializers

--- project/serializers.py
+++ project/serializers.py
@@ -2,9 +2,42 @@ from rest_framework import serializers
 from .models import Project
 from django.contrib.auth.models import User
 
+from rest_framework import serializers
+from .models import Project
+from django.contrib.auth.models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username']
+
 
 class ProjectSerializer(serializers.ModelSerializer):
+    members = UserSerializer(many=True, read_only=True)
+    owner = UserSerializer(read_only=True)
+    member_ids = serializers.PrimaryKeyRelatedField(
+        queryset=User.objects.all(),
+        write_only=True,
+        source='members',
+        many=True,
+        required=False
+    )
+
     class Meta:
         model = Project
-        fields = '__all__'
+        fields = ['id', 'name', 'description', 'created_at', 'updated_at', 'owner', 'members', 'member_ids']
         read_only_fields = ('owner', 'created_at', 'updated_at')
+
+    def create(self, validated_data):
+        members = validated_data.pop('members', [])
+        project = Project.objects.create(**validated_data)
+        project.members.set(members)
+        return project
+
+    def update(self, instance, validated_data):
+        members = validated_data.pop('members', None)
+        instance = super().update(instance, validated_data)
+        if members is not None:
+            instance.members.set(members)
+        return instance
